2c1d42bd6c3be8dca509937d22a3a354c0ca61b6
penny <edoardoperissinotto704@gmail.com>
2026-05-31 19:02:04 +0200

    ???

--- List/List.hpp
+++ List/List.hpp
@@ -2,81 +2,110 @@
 #define LIST_HPP
 
 #include <cstddef>
-#include <iostream>
+#include <iterator>
+// #include <iostream>
 
+namespace tgl {
+
+/**
+ * @brief A generic doubly-linked list implementation.
+ * @tparam T The type of elements stored in the list.
+ */
 template<class T>
 class List 
 {
-  struct Node 
-  {
-    Node(T elem, Node* next = nullptr, Node* prev = nullptr)
-      : next_{ next }
-      , prev_{ prev }
-      , elem_{ elem }
-      { }
-    
-    void insert_before(Node* next) {
-      next->prev_ = this;
-      this->next_ = next;
-    }
+  private:
+    struct Node; 
 
-    void insert_after(Node* prev) {
-      prev->next_ = this;
-      this->prev_ = prev;
-    }
+  public:
+    // Type definitions
+    using value_type      = T;
+    using size_type       = size_t;
+    using reference       = T&;
+    using const_reference = const T&;
 
-    void insert_between(Node* prev, Node* next) {
-      insert_after(prev);
-      insert_before(next);
-    }
-  
-    Node* next_;
-    Node* prev_;
-    T elem_;
-  }; 
+    class iterator 
+    {
+      public:
+        using iterator_category = std::bidirectional_iterator_tag;
+        using value_type        = T;
+        using difference_type   = std::ptrdiff_t;
+        using pointer           = T*;
+        using reference         = T&;
 
-  class iterator 
-  {
-    private:
-      Node* p;
+      private:
+        Node* p;
 
-    public:
-      iterator(Node* it) : p{ it } { }
-      iterator& operator--() { p = p->prev_; return *this; }
-      iterator& operator++() { p = p->next_; return *this; }
-      iterator  operator++(int) { 
-        Node* temp_p = p;
-        p = p->next_;
-        return p;
-      }
-      T& operator*() { return p->elem_; }
-      Node* get_node() const { return p; }
-      bool operator==(const iterator& it) const { return p == it.p; }
-      bool operator!=(const iterator& it) const { return p != it.p; }
-  };
+      public:
+        /**
+         * @brief Constructs an iterator from a node pointer.
+         * @param it Pointer to the node.
+         */
+        iterator(Node* it) : p{ it } { }
 
-  public: 
+        iterator& operator--() { p = p->prev_; return *this; }
+        iterator& operator++() { p = p->next_; return *this; }
+        iterator operator++(int) { 
+          Node* temp_p = p;
+          p = p->next_;
+          return temp_p;
+        }
+
+        reference operator*() { return p->elem_; }
+
+        /**
+         * @brief Retrieves the underlying node pointer.
+         */
+        Node* get_ptr() const { return p; }
+
+        bool operator==(const iterator& it) const { return p == it.p; }
+        bool operator!=(const iterator& it) const { return p != it.p; }
+    };
+
+    /**
+     * @brief Default constructor. Initializes an empty list.
+     */
     List() 
       : head_{ nullptr }
       , tail_{ head_ }
       { }
 
+    /**
+     * @brief Returns an iterator to the beginning of the list.
+     */
     iterator begin() { return iterator(head_); }
-    iterator end() { return iterator(tail_->next_); }
+
+    /**
+     * @brief Returns an iterator to the end of the list.
+     */
+    iterator end() { return iterator(nullptr); }
     
-    // Inserts elem before it
-    //  TO DO
-    iterator insert(iterator& it, const T& elem) {
+    /**
+     * @brief Inserts an element before the specified iterator position.
+     * @param it Iterator indicating the position before which to insert.
+     * @param elem The element to insert.
+     * @return Iterator pointing to the newly inserted element.
+     * @todo Implement the insertion logic.
+     */
+    iterator insert(iterator& it, const_reference elem) {
       return begin();
     }
 
-    // erase element before p
-    //  TO DO
+    /**
+     * @brief Erases the element at the specified iterator position.
+     * @param p Iterator pointing to the element to erase.
+     * @return Iterator pointing to the element following the erased one.
+     * @todo Implement the erase logic.
+     */
     iterator erase(iterator p) {
       return begin();
     }
 
-    void push_back(const T& elem) {        
+    /**
+     * @brief Adds an element to the end of the list.
+     * @param elem The element to append.
+     */
+    void push_back(const_reference elem) {        
       Node* new_node{ new Node(elem) };
       if (size_ == 0) {
         head_ = new_node;
@@ -89,7 +118,11 @@ class List
       size_++;
     }
 
-    void push_front(const T& elem) {
+    /**
+     * @brief Adds an element to the beginning of the list.
+     * @param elem The element to prepend.
+     */
+    void push_front(const_reference elem) {
       Node* new_node{ new Node(elem) };
       if (size_ == 0) {
         head_ = new_node;
@@ -102,6 +135,9 @@ class List
       size_++;
     }
     
+    /**
+     * @brief Removes the last element from the list.
+     */
     void pop_back() {
       if (size_ >= 1) { 
         tail_ = tail_->prev_;
@@ -110,6 +146,9 @@ class List
       }
     }
     
+    /**
+     * @brief Removes the first element from the list.
+     */
     void pop_front() {
       if (size_ >= 1) { 
         head_ = head_->next_;
@@ -118,21 +157,45 @@ class List
       }
     }
 
-    T& front() { return head_->elem_; }
-    T& back()  { return tail_->elem_; }   
+    /**
+     * @brief Accesses the first element of the list.
+     * @return Reference to the first element.
+     */
+    reference front() { return head_->elem_; }
 
-    void print() {
-      for(auto it{ begin()}; it != end(); it++) {
-        std::cout << *it << ' ';
-      }
-    }
-    
+    /**
+     * @brief Accesses the last element of the list.
+     * @return Reference to the last element.
+     */
+    reference back()  { return tail_->elem_; }   
+
   private:
+
+    struct Node 
+    {
+      /**
+       * @brief Constructs a new Node.
+       * @param elem The element to store in the node.
+       * @param next Pointer to the next node (default nullptr).
+       * @param prev Pointer to the previous node (default nullptr).
+       */
+      Node(const_reference elem, Node* next = nullptr, Node* prev = nullptr)
+        : next_{ next }
+        , prev_{ prev }
+        , elem_{ elem }
+        { }
+  
+      Node* next_;
+      Node* prev_;
+      value_type elem_;
+    }; 
+
     Node* head_;
     Node* tail_;
 
-    std::size_t size_{ 0 };
-};
+    size_type size_{ 0 };
+
+}; // List
+}  // tgl
 
 #endif // LIST_HPP
-
---
+++ UnorderedMap/unordered_map.hpp
@@ -0,0 +1,19 @@
+#ifndef UNORDERED_MAP_HPP
+#define UNORDERED_MAP_HPP
+
+#include <cstddef>
+#include <utility>
+#include <stdexcept>
+
+namespace tgl {
+
+template<
+  class Key,
+  class T
+> class unordered_map {
+
+};
+} // tgl::
+
+
+#endif // UNORDERED_MAP_HPP
